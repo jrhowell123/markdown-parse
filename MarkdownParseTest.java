9a4092925e8d1222109a0027b09f939a4ccb4a1e
jrhowell123 <jrhowell@ucsd.edu>
2022-01-26 18:40:46 -0800

    tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,19 @@
 import static org.junit.Assert.*;
+
+import java.beans.Transient;
+
 import org.junit.*;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void testFileOne() throws IOException{
+        String contentsTest = Files.readstring(path.of("test-file.md"));
+        assertequals(list.of("https://something.com","some-page.html"),
+            MarkdownParse.getLinks(contentsTest));
     }
 }
