2e8a86150bc3562427b4c57aabe6de0256acf06b
jrhowell123 <jrhowell@ucsd.edu>
2022-02-09 18:46:51 -0800

    Lab 6

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -3,8 +3,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-
-public class MarkdownParse {
+ 
+public class MarkdownParse{
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then take up to
@@ -15,15 +15,20 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if(nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1){
+                return toReturn;
+            }
+            if((openParen - nextCloseBracket == 1) && (nextOpenBracket == 0 || markdown.indexOf("!", nextOpenBracket-1) != nextOpenBracket-1)){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
         }
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
-		Path fileName = Path.of(args[0]);
-	    String contents = Files.readString(fileName);
-        System.out.println(contents);
+        Path fileName = Path.of(args[0]);
+        String contents = Files.readString(fileName);
+        //System.out.println(contents);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
     }
